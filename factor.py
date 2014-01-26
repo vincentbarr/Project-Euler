d81ffb6e7f9729f18b65c3e725935d0a03508a5f
vincent barr <vincentvbarr@gmail.com>
2014-01-26 18:14:08 -0500

    problem 4 solved

--- factor.py
+++ factor.py
@@ -1,6 +1,18 @@
 # Function to find all factors of an number
+
+# Importing functtols because the reduce function depends on it
 import functools
 
+# Defining factors function
 def factors(n):
+
+# Set is getting rid of duplicates, such as factors of 4 [2, 2]
+# reduce(list.__add__,...) takes the lists of [fac1, fac2] and adds them into one long list
+# [i, n/i] for i in range(1, int(sqrt(n)) +1) if n % i == 0 returns a pair of factors
+# if the remainder when I divide n by the smaller one is zero.
+# (It doesn't need to check the large one, too, it just gets that dividing n by the smaller one)
+
    return set(functools.reduce(list.__add__,
       ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+# Further explanation of this code as it was borrowed as a model for 'efficient' code: http://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
--- p4.py
+++ p4.py
@@ -8,21 +8,27 @@
 # Largest 3 digit number: 999
 # Answer is 5 - 6 digits
 # Since the number cannot start with 0, it cannot end with 0
-# Therefore, it is not divisible by 10. 
+# Therefore, it is not divisible by 10.
 # So, the three digit numbers are [1-9][0-9][1-9], 810 possibilities.
 
-n = 0
-for i in range (1,10):
-	for j in range (0,10):
-		for k in range (1,10):
-			sameNum = (i*100) + (j*10) +k
-			resultSame = str(sameNum*sameNum)
-			if resultSame == resultSame[::-1]:
-				print (sameNum,sameNum)
-			for l in range(1,10):
-				for m in range (0,10):
-					for n in range (1,10):
-						diffNum = (l*100) + (m*10) + n
-						resultDiff = str(sameNum*diffNum)
-						if resultDiff == resultDiff[::-1]:
-							print (resultDiff, diffNum, sameNum)
+def is_palindrome(i):
+	return str(i) == str(i)[::-1]
+
+max_palindrome = 0
+
+# Starting from 101 because it can't end in 0
+for i in range(1, 999):
+# first pass: for range, (101, 105): i is 101
+# second pass: i is 102
+#	Test: print('i is', i)
+	for j in range(i, 999):
+# first pass: for range (101, 105) j is i + 1, so 102
+# j is i + 2, so 103
+# j is i +3, so 104 (runs up to last integer of range -1 (so, 104))
+# second page: j is 103, 104
+# Test: print('J is', j)
+		p = i * j
+		if is_palindrome(p) and p > max_palindrome:
+			max_palindrome = p
+
+print (max_palindrome)
